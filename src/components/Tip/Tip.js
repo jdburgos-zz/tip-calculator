d7bfe07a46c4f38a6b9aed119cc86e16bd2c6f1a
Juan david burgos oquendo <j.dburgos@hotmail.com>
2021-08-01 08:55:37 -0500

    feat: add custom tip styles

--- src/components/Tip/Tip.js
+++ src/components/Tip/Tip.js
@@ -1,30 +1,49 @@
 /** React core **/
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
+
+/** Components **/
+import Input from '../Ui/Input/Input';
 
 /** Styles **/
 import styles from './Tip.module.scss';
 
 const Tip = props => {
   const [selected, setSelected] = useState(false);
+  const [error, setError] = useState(false);
+  const customTipRef = useRef();
 
   const selectTipHandler = () => {
     setSelected(prevState => !prevState);
   };
 
+  const customTipHandler = () => {
+    const customTipValue = customTipRef.current.value.trim();
+
+    setError(customTipValue === '');
+  };
+
   const selectedClass = selected ? styles['tip--selected'] : '';
   const classes = `${styles['tip']} ${selectedClass}`.trim();
-  let content = `${props.tip.value}%`;
+  let content = <span onClick={selectTipHandler} className={classes}>{`${props.tip.value}%`}</span>;
 
   if (props.tip.type === 'custom') {
-    content = <input className={styles['tip__custom']} type="number" />;
+    content = (
+      <Input
+        className={styles['tip__custom']}
+        ref={customTipRef}
+        input={{
+          id: 'custom-tip-input',
+          type: 'number',
+          placeholder: 'Custom',
+        }}
+        error={error}
+        onChange={customTipHandler}
+      />
+    );
   }
 
-  return (
-    <span onClick={selectTipHandler} className={classes}>
-      {content}
-    </span>
-  );
+  return content;
 };
 
 Tip.propTypes = {
--- src/components/Ui/Input/Input.js
+++ src/components/Ui/Input/Input.js
@@ -9,17 +9,23 @@ const Input = React.forwardRef((props, ref) => {
   const errorClass = props.error ? styles.error : '';
   const className = props.className ? props.className : '';
   const classes = `${styles.input} ${className} ${errorClass}`.trim();
+  const label = (
+    <label className={styles['input__label']} htmlFor={props.input.id}>
+      {props.label}
+    </label>
+  );
+  const icon = (
+    <img
+      className={styles['input__icon']}
+      src={`${process.env.PUBLIC_URL}/images/${props.icon}.svg`}
+      alt={`${props.icon}`}
+    />
+  );
 
   return (
     <div className={styles['input__container']}>
-      <label className={styles['input__label']} htmlFor={props.input.id}>
-        {props.label}
-      </label>
-      <img
-        className={styles['input__icon']}
-        src={`${process.env.PUBLIC_URL}/images/${props.icon}.svg`}
-        alt={`${props.icon}`}
-      />
+      {props.label && label}
+      {props.icon && icon}
       <input onChange={props.onChange} className={classes} ref={ref} {...props.input} />
     </div>
   );
